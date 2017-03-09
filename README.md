feb1937ac0eea51f0e9702a2478541e8fdaf5e60
Logan Call <logancall.dev@gmail.com>
2017-03-09 14:31:50 -0700

    Initial Commit. Add planned program options and Readme

---
+++ README.md
@@ -0,0 +1,30 @@
+# Anione
+An NPM modules to ease the repetitiveness that comes with coding HTML5 banner ads.
+
+# Generated Project Structure
+* ani-conf.json
+* assets
+	* failovers
+		* (project-size.jpg)
+		* (project-size.png)
+		...
+	* images
+		* (layer-size.jpg)
+		* (layer-size.png)
+		...
+* banners
+	* (size)
+		* (size.html)
+		* (size.scss)
+		* (size.js)
+	* (size)
+		* (size.html)
+		* (size.scss)
+		* (some-library.js)
+		* (size.js)
+	* (size)
+		* (size.html)
+		* (some-library.scss)
+		* (size.scss)
+		* (size.js)
+	...
---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node --harmony
+"use strict";
+
+const program = require("commander");
+const co = require("co");
+const prompt = require("co-prompt");
+
+program
+	.version("0.0.1")
+	.command("init", "Builds Anione project directory structure, config and template files.")
+	.command("one", "Using the template file, generates the first banner")
+	.command("resize", "Resize your first banner into all remaining sizes selected during configuration. These can also be found in your ani-conf.json file.")
+	.command("preview", "Generate a drag n' drop preview webpage to showcase the banners.")
+	.command("handoff", "Zip/Package/Compress the Banner-Ads for ad-network delivery.")
+	.command("size <a/r> <size>", "Add or remove a banner size. -a for Add, -r for Remove")
+	.command("vendor <a/r> <name>", "Add or remove a banner vendor. -a for Add, -r for Remove")
+	.parse(process.argv);
