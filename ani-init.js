5ba2ce1e67a4db282f566300b2722ac212e39113
Logan Call <logancall.dev@gmail.com>
2017-03-23 12:33:20 -0600

    Finish ani init command refactor

--- ani-init.js
+++ ani-init.js
@@ -4,42 +4,82 @@ const 	Promise = require("bluebird"),
 				fs = Promise.promisifyAll(require("fs")),
 				inquirer = require("inquirer"),
 				colors = require("colors"),
-				sizeOptions = require("./assets/conf-options/sizes.json"),
-				vendorOptions = require("./assets/conf-options/vendors.json"),
-				_ = require("./utils"),
-				configQuestions = [
-				{
-					type: "input",
-					name: "project",
-					message: "Project Name? This may be the concept, or a combination of client and concept.\n",
-					validate: (answer) => {
-						if (answer.length < 1) return "You must enter the Project Name.\n";
-						return true;
-					}
-				}, {
-					type: "checkbox",
-					message: "Select Banner Sizes. If you do not see all of the required sizes for your project here, you can add them after the project initialization by running `ani size -a widthXHeight`, or by editing ani-conf.js directly.\n",
-					name: "sizes",
-					choices: sizeOptions,
-					validate: (answer) => {
-						if (answer.length < 1) return "You must choose at least one size.\n";
-						return true;
-					}
-				}, {
-					type: "checkbox",
-					message: "Select Vendors. If you do not see all of the required sizes for your project here, you can add them after the project initialization by running `ani vendor -a vendorName`, or by editing ani-conf.js directly.\n",
-					name: "vendors",
-					choices: Object.keys(vendorOptions),
-					validate: (answer) => {
-						if (answer.length < 1) return "You must choose at least one vendor.\n";
-						return true;
-					}
+				$ = require("./utils"),
+				configQuestions = (vendors, sizes) => {
+					return [
+						{
+							type: "input",
+							name: "project",
+							message: "Project Name? This may be the concept, or a combination of client and concept.\n",
+							validate: (answer) => {
+								if (answer.length < 1) return "You must enter the Project Name.\n";
+								return true;
+							}
+						}, {
+							type: "checkbox",
+							message: "Select Banner Sizes. If you do not see all of the required sizes for your project here, you can add them after the project initialization by running `ani size -a widthXHeight`, or by editing ani-conf.js directly.\n",
+							name: "sizes",
+							choices: sizes,
+							validate: (answer) => {
+								if (answer.length < 1) return "You must choose at least one size.\n";
+								return true;
+							}
+						}, {
+							type: "checkbox",
+							message: "Select Vendors. If you do not see all of the required sizes for your project here, you can add them after the project initialization by running `ani vendor -a vendorName`, or by editing ani-conf.js directly.\n",
+							name: "vendors",
+							choices: Object.keys(vendors),
+							validate: (answer) => {
+								console.log("answer", answer)
+								if (answer.length < 1) return "You must choose at least one vendor.\n";
+								return true;
+							},
+							filter: (vendorKeys) => {
+								let fullVendors = {};
+								for (let vendorKey of vendorKeys) {
+									fullVendors[vendorKey] = vendors[vendorKey];
+								}
+								return fullVendors;
+							}
+						}
+					];
+					return Promise.resolve();
 				}
-			];
+
+let 	vendors = (async () => await $.read_path(__dirname + "/assets/conf-options/vendors.json"))(),
+			sizes = (async () => await $.read_path(__dirname + "/assets/conf-options/sizes.json"))();
+
+
+
 
 exports.init = async function () {
+	const config_file = await $.read_path("./ani-conf.json"),
+				readme_file = await $.read_path("./README.md"),
+				scrubber_file = await $.read_path("./README.md");
 
-    
+	if (config_file) return console.warn(colors.yellow("Your project has already been initialized. To re-initialize, first delete ani-conf.json."));
+
+	try {
+		await $.build_directories();
+		vendors = JSON.parse(await vendors);
+		sizes = JSON.parse(await sizes);
+		let config = await inquirer.prompt(await configQuestions(vendors, sizes));
+		config = JSON.stringify(config, null, 2);
+
+		await fs.writeFileAsync("ani-conf.json", config);
+
+		$.handle_success(`${config}\n\nHere is your project config. If this is wrong, you can edit 'ani-conf.json' manually.\n`)
+
+		// Copy README file to project instance
+		if (!readme_file) fs.createReadStream(__dirname + `/README.md`).pipe(fs.createWriteStream("./README.md"));
+
+		// Copy scrubber.js file to project instance
+		if (!scrubber_file) fs.createReadStream(__dirname + `/assets/scrubber.js`) .pipe(fs.createWriteStream("./.anione/scrubber.js"));
+
+	} catch (e) {
+		console.log(e)
+		$.handle_error(e, "Could not generate config.")
+	}
 };
 
 exports.init();
--- utils.js
+++ utils.js
@@ -1,11 +1,72 @@
 "use strict";
 
+const Promise = require("bluebird"),
+			fs = Promise.promisifyAll(require("fs")),
+			colors = require("colors");
+
 module.exports = {
-	isHidden (filename) {
+	handle_error(e, default_message) { // We don't want to just throw an error object at a user.. so when we reject promises we can give useful error messages. But in the event that it's not an error we're aware of, we still want a default message for that specific ani command.
+		if(typeof(e) !== "string") e = default_message;
+		console.error(colors.red(`Error! ${e}`));
+	},
+	handle_success(message) {
+		console.log(colors.green(message));
+	},
+	is_hidden (filename) {
 		if (/^\./.test(filename)) {
 			return true;
 		} else {
 			return false;
 		}
+	},
+	read_path: async function(filePath) { // return file if exists, return false if it doesn't
+		try {
+			return Promise.resolve(await fs.readFileAsync(filePath, "utf8"));
+		} catch (e) {
+			return Promise.resolve(false)
+		}
+	},
+	build_directories: async function() {
+		for (let i = 0; i <= this.paths.build_directories.length; i++) {
+			const directory = this.paths.build_directories[i];
+			if (directory) {
+				try {
+					const directoryStat = await this.read_path(directory);
+					if (!directoryStat) await fs.mkdirAsync(directory);
+				} catch (e) {
+					return console.warn(colors.yellow(`Couldn't create project directory, ${directory}`));
+				}
+			}
+		}
+		return Promise.resolve();
+	},
+	paths: {
+		build_directories: [ // these are the directories that are generated during `ani init`
+			"./assets",
+			"./.anione",
+			"./assets/failovers",
+			"./assets/images",
+			"./assets/libs-js",
+			"./assets/libs-css",
+			"./banners",
+			"./preview",
+			"./preview/assets",
+			"./preview/banners",
+		],
+		directories: {
+			"scripts": "./assets/libs-js/",
+			"styles": "./assets/libs-css/",
+			"images": "../assets/images/",
+		},
+		template: {
+			"banner" : "templates/banner.pug",
+			"dev" : "templates/dev.pug",
+			"preview" : "templates/preview.pug",
+		},
+		watch: [
+			"banners/**", 
+			"assets/**", 
+			"index.html",
+		],
 	}
 };
