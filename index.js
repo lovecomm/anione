d34f71912d4eb33907ff681a9af7b32c33f0bd03
Logan Call <logancall.dev@gmail.com>
2017-04-05 15:53:19 -0600

    Fix bug in dev template that was causing an endless loop in a recursive function when there were no scripts w/src

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const co = require("co");
 const prompt = require("co-prompt");
 
 program
-	.version("1.1.0")
+	.version("1.1.1")
 	.command("init", "Builds Anione project directory structure and config.")
 	.command("one", "Generates first banner from template.")
 	.command("resize", "Resize your first banner into all remaining sizes selected during configuration. These can also be found in your ani-conf.json file.")
